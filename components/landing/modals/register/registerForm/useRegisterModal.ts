a3b257c9439a99c780cea6b819eedadd75003608
nnataali <natalia.charkviani.1@btu.edu.ge>
2023-01-12 16:43:34 +0400

    feat: create function to register a user

--- components/landing/modals/register/registerForm/useRegisterModal.ts
+++ components/landing/modals/register/registerForm/useRegisterModal.ts
@@ -1,6 +1,7 @@
 import useTranslation from 'next-translate/useTranslation';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { RegisterModalTypes } from 'types';
+import axios from 'axios';
 
 const useRegisterModal = () => {
   const { t } = useTranslation('common');
@@ -12,8 +13,21 @@ const useRegisterModal = () => {
     formState: { errors },
   } = useForm<RegisterModalTypes>({ mode: 'all' });
 
-  const onSubmit: SubmitHandler<RegisterModalTypes> = (data) =>
-    console.log(data);
+  const onSubmit: SubmitHandler<RegisterModalTypes> = (data) => {
+    axios
+      .post('/api/register', {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+        confirm_password: data.confirm_password,
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return { t, register, getValues, handleSubmit, control, errors, onSubmit };
 };
